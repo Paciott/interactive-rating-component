3aaac2d411bf7223e9ac06b8434849c61b7ba10b
andrea <pacittoandrea@gmail.com>
2022-09-19 19:47:48 +0200

    update index.js

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const thankYou = document.querySelector(".thank-you");
 const submitButton = document.querySelector(".submit");
 const ratingNums = document.querySelectorAll(".button");
 const yourRate = document.getElementById("rate");
+let rating = null;
 
 submitButton.addEventListener("click", () => {
-  thankYou.classList.remove("hidden");
-  ratingState.classList.add("hidden");
+  if (rating === null || rating < 1 || rating > 5) {
+    alert("Please pick a number");
+  } else {
+    yourRate.innerText = rating;
+    thankYou.classList.remove("hidden");
+    ratingState.classList.add("hidden");
+  }
 });
 
 ratingNums.forEach((ratingNum) => {
   ratingNum.addEventListener("click", () => {
-    yourRate.innerHTML = ratingNum.innerText;
+    rating = parseInt(ratingNum.innerText);
+    // yourRate.innerText = ratingNum.innerText;
   });
 });
